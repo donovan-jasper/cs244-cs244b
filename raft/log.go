89aae767a73d6d4b56de2ddebb3e3aa24c650540
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2024-05-21 23:53:12 -0700

    Need to add persistence

--- raft/log.go
+++ raft/log.go
@@ -1,16 +1,33 @@
-package log
+package raftlog
+
+//accept and convert protobuf into LogEntry struct
+// convert LogEntry into protobuf
+
+// CALL 3 ENTRIES:
+// append a LogEntry Protobuf (which will be a struct with protobuf functions)
+// read a LogEntry (give an index, return)
+// delete a LogEntry (give an index, return)
 
 // creating raft log entry
-type logEntry struct {
-	term    int
-	index   int         // ensure ordering
-	command interface{} // change to bytes?
+type LogEntry struct {
+	term    int    // term when entry was received by leader
+	index   int    // ensure ordering
+	command []byte // change to bytes?
 }
 
-func createLogEntry(term int, index int, command interface{}) *logEntry {
-	return &logEntry{
-		term:    term,
-		index:   index,
-		command: command,
-	}
+type RaftLog struct {
+	entries []LogEntry
+}
+
+// might return on failure
+func (r *RaftLog) AppendEntry(entry LogEntry) {
+	r.entries = append(r.entries, entry)
+}
+
+func (r *RaftLog) GetEntry(index int) LogEntry {
+	return r.entries[index]
+}
+
+func (r *RaftLog) DeleteEntry(index int) {
+	r.entries = append(r.entries[:index], r.entries[index+1:]...)
 }
