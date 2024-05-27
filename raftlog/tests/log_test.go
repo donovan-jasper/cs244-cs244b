b3f65b42a17782143a0eb4c82e2f331e73d95442
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2024-05-27 16:42:34 -0700

    Fixed test to save and load WAL

--- raftlog/tests/log_test.go
+++ raftlog/tests/log_test.go
@@ -1,11 +1,15 @@
 package tests
 
 import (
+	"encoding/binary"
+	"io"
 	"os"
 	"path/filepath"
 	"raftlog"
 	"testing"
 	"time"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func TestCreateRaftLog(t *testing.T) {
@@ -52,6 +56,29 @@ func TestAppendEntry(t *testing.T) {
 		t.Errorf("Failed to get file info")
 	}
 	if fileInfo.Size() == 0 {
-		t.Errorf("Failed to write entry to WAL")
+		t.Errorf("Failed to write entry to WAL, file size is 0")
+	}
+
+	// print contents of file
+	file, err := os.Open(filepath)
+	if err != nil {
+		t.Errorf("Failed to open file")
+	}
+	defer file.Close()
+	var chunksize uint32
+	newEntry := &raftlog.LogEntry{}
+	binary.Read(file, binary.LittleEndian, &chunksize)
+	data := make([]byte, chunksize)
+	if _, err := io.ReadFull(file, data); err != nil {
+		t.Errorf("Failed to read data")
+	}
+	// decode data
+	if err := proto.Unmarshal(data, newEntry); err != nil {
+		t.Error("failed to unmarshal entry", err)
+	}
+
+	// Check if the entry was written correctly to the WAL
+	if newEntry.Term != entry.Term || newEntry.Index != entry.Index || newEntry.Command != entry.Command {
+		t.Errorf("Failed to write entry correctly to WAL. Expected: %v, Got: %v", entry, newEntry)
 	}
 }
